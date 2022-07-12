5c86452187f035b5b8bfc8981c17d8f683e84a95
Zibbp <zibbp@protonmail.com>
2022-07-12 14:46:28 -0500

    Feat: Seed database with initial user

--- internal/database/database.go
+++ internal/database/database.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 	"github.com/zibbp/ganymede/ent"
+	"github.com/zibbp/ganymede/internal/utils"
+	"golang.org/x/crypto/bcrypt"
 	"os"
 )
 
@@ -37,5 +40,33 @@ func NewDatabase() (*Database, error) {
 		return nil, err
 	}
 
+	isSeeded := viper.Get("db_seeded").(bool)
+	if !isSeeded {
+		log.Debug().Msg("seeding database")
+		if err := seedDatabase(client); err != nil {
+			return nil, err
+		}
+		viper.Set("db_seeded", true)
+		err := viper.WriteConfig()
+		if err != nil {
+			return nil, fmt.Errorf("error writing config: %v", err)
+		}
+	}
+
 	return &Database{Client: client}, nil
+}
+
+func seedDatabase(client *ent.Client) error {
+
+	// Create initial user
+	hashPass, err := bcrypt.GenerateFromPassword([]byte("ganymede"), 14)
+	if err != nil {
+		return fmt.Errorf("error hashing password: %v", err)
+	}
+	_, err = client.User.Create().SetUsername("admin").SetPassword(string(hashPass)).SetRole(utils.Role("admin")).Save(context.Background())
+	if err != nil {
+		return fmt.Errorf("error creating user: %v", err)
+	}
+
+	return nil
 }
