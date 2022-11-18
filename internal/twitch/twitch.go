c8c6de1702abf9ada8c0386170cd54fe169bbd73
Zibbp <zibbp@protonmail.com>
2022-11-17 19:28:31 -0600

    feat(twitch): check access token function

--- internal/twitch/twitch.go
+++ internal/twitch/twitch.go
@@ -262,3 +262,25 @@ func (s *Service) GetStreams(queryParams string) (Stream, error) {
 
 	return streamResponse, nil
 }
+
+func CheckUserAccessToken(accessToken string) error {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://id.twitch.tv/oauth2/validate", nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %v", err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("OAuth %s", accessToken))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to check access token: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to check access token: %v", resp)
+	}
+
+	return nil
+}
