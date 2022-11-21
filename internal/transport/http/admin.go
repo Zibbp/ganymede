62367e12a2ef97032b5f89e8177c49dad4aeecca
Zibbp <zibbp@protonmail.com>
2022-11-20 19:49:48 -0600

    feat(admin): info route - returns version, uptime, and program versions

--- internal/transport/http/admin.go
+++ internal/transport/http/admin.go
@@ -8,12 +8,21 @@ import (
 
 type AdminService interface {
 	GetStats(c echo.Context) (admin.GetStatsResp, error)
+	GetInfo(c echo.Context) (admin.InfoResp, error)
 }
 
 func (h *Handler) GetStats(c echo.Context) error {
 	resp, err := h.Service.AdminService.GetStats(c)
 	if err != nil {
 		return err
+	}
+	return c.JSON(http.StatusOK, resp)
+}
+
+func (h *Handler) GetInfo(c echo.Context) error {
+	resp, err := h.Service.AdminService.GetInfo(c)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusOK, resp)
 }
