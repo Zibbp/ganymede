002bfb180845ea70ea1c465fda02f21078143770
Zibbp <zibbp@protonmail.com>
2023-01-08 20:59:17 +0000

    feat(dev): add memory function for development

---
+++ internal/utils/utils.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
