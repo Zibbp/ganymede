771b9877093b11f14f744228ef6bb53381a7e75f
Zibbp <zibbp@zibbp.net>
2025-02-20 03:40:34 +0000

    fix(task): overhaul storage migration task

--- internal/task/task.go
+++ internal/task/task.go
@@ -108,179 +108,274 @@ func (s *Service) StartTask(ctx context.Context, task string) error {
 	return nil
 }
 
+// renameOperation represents a single successful rename (old->new).
+type renameOperation struct {
+	oldPath string
+	newPath string
+}
+
+// rollbackRenames will undo the renames in reverse order.
+func rollbackRenames(ops []renameOperation) {
+	for i := len(ops) - 1; i >= 0; i-- {
+		op := ops[i]
+		if err := os.Rename(op.newPath, op.oldPath); err != nil {
+			log.Error().Err(err).Msgf("error rolling back rename from %s to %s", op.newPath, op.oldPath)
+		} else {
+			log.Info().Msgf("rolled back rename from %s to %s", op.newPath, op.oldPath)
+		}
+	}
+}
+
 func (s *Service) StorageMigration() error {
-	// Get all videos in db
+	// Get all videos from the database.
 	videos, err := s.Store.Client.Vod.Query().WithChannel().All(context.Background())
 	if err != nil {
 		return fmt.Errorf("error getting videos: %v", err)
 	}
 
-	// Loop through videos and move them to new storage
+	// Loop through each video.
 	for _, video := range videos {
-
-		// Populate templates
+		// Prepare the template input.
 		storageTemplateInput := archive.StorageTemplateInput{
 			UUID:    video.ID,
 			ID:      video.ExtID,
 			Channel: video.Edges.Channel.Name,
 			Title:   video.Title,
 			Type:    string(video.Type),
-			Date:    video.CreatedAt.Format("2006-01-02"),
+			Date:    video.StreamedAt.Format("2006-01-02"),
 		}
+
+		// Get folder and file names.
 		folderName, err := archive.GetFolderName(video.ID, storageTemplateInput)
 		if err != nil {
-			log.Error().Err(err).Msgf("Error getting folder name for video %s", video.ID)
+			log.Error().Err(err).Msgf("error getting folder name for video %s", video.ID)
 			continue
 		}
 		fileName, err := archive.GetFileName(video.ID, storageTemplateInput)
 		if err != nil {
-			log.Error().Err(err).Msgf("Error getting file name for video %s", video.ID)
+			log.Error().Err(err).Msgf("error getting file name for video %s", video.ID)
 			continue
 		}
 
-		// Extract parts of path
-		// Use video path as that will always be available
-		tmpRootFolder := strings.SplitN(video.VideoPath, "/", 6)[0:4]
-		// Add array of strings together seperated by /
-		oldRootFolderPath := strings.Join(tmpRootFolder, "/")
+		// Extract old root folder (using video path as reference if not using HLS).
+		var oldRootFolderPath string
+		if video.VideoHlsPath != "" {
+			oldRootFolderPath = path.Dir(video.ThumbnailPath)
+		} else {
+			oldRootFolderPath = path.Dir(video.VideoPath)
+		}
 
 		envConfig := config.GetEnvConfig()
-		newRootFolderPath := fmt.Sprintf("/%s/%s/%s", envConfig.VideosDir, video.Edges.Channel.Name, folderName)
+		newRootFolderPath := fmt.Sprintf("%s/%s/%s", envConfig.VideosDir, video.Edges.Channel.Name, folderName)
 
-		// Rename files first
-		// Video
+		// We'll record each successful rename here.
+		var renames []renameOperation
+
+		// safeRename is a helper to perform the os.Rename and record the operation.
+		safeRename := func(oldPath, newPath string) error {
+			err := os.Rename(oldPath, newPath)
+			if err != nil {
+				// If the error is that the target already exists, we consider that acceptable.
+				if os.IsExist(err) {
+					return nil
+				}
+				return fmt.Errorf("failed to rename %s to %s: %w", oldPath, newPath, err)
+			}
+			renames = append(renames, renameOperation{oldPath: oldPath, newPath: newPath})
+			return nil
+		}
+
+		if err := os.MkdirAll(newRootFolderPath, 0755); err != nil {
+			return fmt.Errorf("failed to create destination directory: %w", err)
+		}
+
+		// Perform renames for each file if a path is provided.
+		// Video file
 		if video.VideoPath != "" {
 			ext := path.Ext(video.VideoPath)
 			if ext == ".m3u8" {
-				parts := strings.Split(video.VideoPath, "/")
-				path := strings.Join(parts[:len(parts)-1], "/")
-				err := os.Rename(path, fmt.Sprintf("%s/%s-video_hls", oldRootFolderPath, fileName))
-				if err != nil {
-					if os.IsExist(err) {
-					} else {
-						log.Error().Err(err).Msgf("Error renaming %s to %s. Skipping this video", path, fmt.Sprintf("%s/%s-video_hls", oldRootFolderPath, fileName))
-						continue
-					}
-				}
-				_, err = video.Update().SetVideoPath(fmt.Sprintf("%s/%s-video_hls/%s-video.m3u8", newRootFolderPath, fileName, video.ExtID)).Save(context.Background())
-				if err != nil {
-					log.Error().Err(err).Msgf("Error updating video path for video %s", video.ID)
+				oldHlsVideoRootPath := path.Dir(video.VideoPath)
+				// oldSpriteThumbnailRootPath := fmt.Sprintf("%s/sprites", spriteThumbnailRoot)
+				newHlsVideoRootPath := fmt.Sprintf("%s/%s-video_hls", newRootFolderPath, fileName)
+				if err := safeRename(oldHlsVideoRootPath, newHlsVideoRootPath); err != nil {
+					log.Error().Err(err).Msgf("error renaming hls video directory for video %s", video.ID)
+					rollbackRenames(renames)
 					continue
 				}
 			} else {
-				err := os.Rename(video.VideoPath, fmt.Sprintf("%s/%s-video%s", oldRootFolderPath, fileName, ext))
-				if err != nil {
-					if os.IsExist(err) {
-					} else {
-						log.Error().Err(err).Msgf("Error renaming %s to %s. Skipping this video", video.VideoPath, fmt.Sprintf("%s/%s-video_hls", oldRootFolderPath, fileName))
-						continue
-					}
-				}
-				_, err = video.Update().SetVideoPath(fmt.Sprintf("%s/%s-video%s", newRootFolderPath, fileName, ext)).Save(context.Background())
-				if err != nil {
-					log.Error().Err(err).Msgf("Error updating video path for video %s", video.ID)
+				newVideoPath := fmt.Sprintf("%s/%s-video%s", newRootFolderPath, fileName, ext)
+				if err := safeRename(video.VideoPath, newVideoPath); err != nil {
+					log.Error().Err(err).Msgf("error renaming video file for video %s", video.ID)
+					rollbackRenames(renames)
 					continue
 				}
 			}
 		}
+
 		// Thumbnail
 		if video.ThumbnailPath != "" {
-			ext := path.Ext(video.ThumbnailPath)
-			err := os.Rename(video.ThumbnailPath, fmt.Sprintf("%s/%s-thumbnail%s", oldRootFolderPath, fileName, ext))
-			if err != nil {
-				if os.IsExist(err) {
-				} else {
-					log.Error().Err(err).Msgf("Error renaming %s to %s.", video.ThumbnailPath, fmt.Sprintf("%s/%s-thumbnail%s", oldRootFolderPath, fileName, ext))
-				}
-
-			}
-			_, err = video.Update().SetThumbnailPath(fmt.Sprintf("%s/%s-thumbnail%s", newRootFolderPath, fileName, ext)).Save(context.Background())
-			if err != nil {
-				log.Error().Err(err).Msgf("Error updating thumbnail path for video %s", video.ID)
+			newPath := fmt.Sprintf("%s/%s-thumbnail%s", newRootFolderPath, fileName, path.Ext(video.ThumbnailPath))
+			if err := safeRename(video.ThumbnailPath, newPath); err != nil {
+				log.Error().Err(err).Msgf("error renaming thumbnail for video %s", video.ID)
+				rollbackRenames(renames)
 				continue
 			}
 		}
+
 		// Web Thumbnail
 		if video.WebThumbnailPath != "" {
-			ext := path.Ext(video.WebThumbnailPath)
-			err := os.Rename(video.WebThumbnailPath, fmt.Sprintf("%s/%s-web_thumbnail%s", oldRootFolderPath, fileName, ext))
-			if err != nil {
-				if os.IsExist(err) {
-				} else {
-					log.Error().Err(err).Msgf("Error renaming %s to %s.", video.WebThumbnailPath, fmt.Sprintf("%s/%s-web_thumbnail%s", oldRootFolderPath, fileName, ext))
-				}
-
-			}
-			_, err = video.Update().SetWebThumbnailPath(fmt.Sprintf("%s/%s-web_thumbnail%s", newRootFolderPath, fileName, ext)).Save(context.Background())
-			if err != nil {
-				log.Error().Err(err).Msgf("Error updating web thumbnail path for video %s", video.ID)
+			newPath := fmt.Sprintf("%s/%s-web_thumbnail%s", newRootFolderPath, fileName, path.Ext(video.WebThumbnailPath))
+			if err := safeRename(video.WebThumbnailPath, newPath); err != nil {
+				log.Error().Err(err).Msgf("error renaming web thumbnail for video %s", video.ID)
+				rollbackRenames(renames)
 				continue
 			}
 		}
-		// Chat Path
+
+		// Chat file
 		if video.ChatPath != "" {
-			ext := path.Ext(video.ChatPath)
-			err := os.Rename(video.ChatPath, fmt.Sprintf("%s/%s-chat%s", oldRootFolderPath, fileName, ext))
-			if err != nil {
-				if os.IsExist(err) {
-				} else {
-					log.Error().Err(err).Msgf("Error renaming %s to %s.", video.ChatPath, fmt.Sprintf("%s/%s-chat%s", oldRootFolderPath, fileName, ext))
-				}
-
-			}
-			_, err = video.Update().SetChatPath(fmt.Sprintf("%s/%s-chat%s", newRootFolderPath, fileName, ext)).Save(context.Background())
-			if err != nil {
-				log.Error().Err(err).Msgf("Error updating chat path for video %s", video.ID)
+			newPath := fmt.Sprintf("%s/%s-chat%s", newRootFolderPath, fileName, path.Ext(video.ChatPath))
+			if err := safeRename(video.ChatPath, newPath); err != nil {
+				log.Error().Err(err).Msgf("error renaming chat file for video %s", video.ID)
+				rollbackRenames(renames)
 				continue
 			}
 		}
-		// Chat Video Path
+
+		// Chat video file (note: same suffix as chat)
 		if video.ChatVideoPath != "" {
-			ext := path.Ext(video.ChatVideoPath)
-			err := os.Rename(video.ChatVideoPath, fmt.Sprintf("%s/%s-chat%s", oldRootFolderPath, fileName, ext))
-			if err != nil {
-				if os.IsExist(err) {
-				} else {
-					log.Error().Err(err).Msgf("Error renaming %s to %s.", video.ChatVideoPath, fmt.Sprintf("%s/%s-chat%s", oldRootFolderPath, fileName, ext))
-				}
-
-			}
-			_, err = video.Update().SetChatVideoPath(fmt.Sprintf("%s/%s-chat%s", newRootFolderPath, fileName, ext)).Save(context.Background())
-			if err != nil {
-				log.Error().Err(err).Msgf("Error updating chat video path for video %s", video.ID)
+			newPath := fmt.Sprintf("%s/%s-chat%s", newRootFolderPath, fileName, path.Ext(video.ChatVideoPath))
+			if err := safeRename(video.ChatVideoPath, newPath); err != nil {
+				log.Error().Err(err).Msgf("error renaming chat video for video %s", video.ID)
+				rollbackRenames(renames)
 				continue
 			}
 		}
-		// Info Path
+
+		// Live Chat file
+		if video.LiveChatPath != "" {
+			newPath := fmt.Sprintf("%s/%s-live-chat%s", newRootFolderPath, fileName, path.Ext(video.LiveChatPath))
+			if err := safeRename(video.LiveChatPath, newPath); err != nil {
+				log.Error().Err(err).Msgf("error renaming live chat for video %s", video.ID)
+				rollbackRenames(renames)
+				continue
+			}
+		}
+
+		// Live Chat Convert file
+		if video.LiveChatConvertPath != "" {
+			newPath := fmt.Sprintf("%s/%s-live-chat-convert%s", newRootFolderPath, fileName, path.Ext(video.LiveChatConvertPath))
+			if err := safeRename(video.LiveChatConvertPath, newPath); err != nil {
+				log.Error().Err(err).Msgf("error renaming live chat convert for video %s", video.ID)
+				rollbackRenames(renames)
+				continue
+			}
+		}
+
+		// Info file
 		if video.InfoPath != "" {
-			ext := path.Ext(video.InfoPath)
-			err := os.Rename(video.InfoPath, fmt.Sprintf("%s/%s-info%s", oldRootFolderPath, fileName, ext))
-			if err != nil {
-				if os.IsExist(err) {
-				} else {
-					log.Error().Err(err).Msgf("Error renaming %s to %s.", video.ChatVideoPath, fmt.Sprintf("%s/%s-info%s", oldRootFolderPath, fileName, ext))
-				}
-
-			}
-			_, err = video.Update().SetInfoPath(fmt.Sprintf("%s/%s-info%s", newRootFolderPath, fileName, ext)).Save(context.Background())
-			if err != nil {
-				log.Error().Err(err).Msgf("Error updating info path for video %s", video.ID)
+			newPath := fmt.Sprintf("%s/%s-info%s", newRootFolderPath, fileName, path.Ext(video.InfoPath))
+			if err := safeRename(video.InfoPath, newPath); err != nil {
+				log.Error().Err(err).Msgf("error renaming info file for video %s", video.ID)
+				rollbackRenames(renames)
 				continue
 			}
 		}
 
-		// Rename root video folder
-		err = os.Rename(oldRootFolderPath, newRootFolderPath)
-		if err != nil {
-			if os.IsExist(err) {
-
-			} else {
-				log.Error().Err(err).Msgf("Error renaming %s to %s.", oldRootFolderPath, newRootFolderPath)
+		// Caption file
+		if video.CaptionPath != "" {
+			newPath := fmt.Sprintf("%s/%s-caption%s", newRootFolderPath, fileName, path.Ext(video.CaptionPath))
+			if err := safeRename(video.CaptionPath, newPath); err != nil {
+				log.Error().Err(err).Msgf("error renaming caption for video %s", video.ID)
+				rollbackRenames(renames)
+				continue
 			}
-
 		}
 
-		log.Info().Msgf("Migrated video %s to new storage template", video.ID)
+		// Sprite thumbnails directory
+		if len(video.SpriteThumbnailsImages) > 0 {
+			spriteThumbnailRoot := strings.Split(video.SpriteThumbnailsImages[0], "/sprites")[0]
+			oldSpriteThumbnailRootPath := fmt.Sprintf("%s/sprites", spriteThumbnailRoot)
+			newSpriteThumbnailRootPath := fmt.Sprintf("%s/sprites", newRootFolderPath)
+			if err := safeRename(oldSpriteThumbnailRootPath, newSpriteThumbnailRootPath); err != nil {
+				log.Error().Err(err).Msgf("error renaming sprite thumbnails directory for video %s", video.ID)
+				rollbackRenames(renames)
+				continue
+			}
+		}
+
+		// Begin a DB transaction to update the file paths.
+		tx, err := s.Store.Client.Tx(context.Background())
+		if err != nil {
+			log.Error().Err(err).Msg("error starting transaction")
+			rollbackRenames(renames)
+			continue
+		}
+
+		// Build the update query with the new paths.
+		update := tx.Vod.UpdateOne(video)
+		if video.VideoPath != "" {
+			ext := path.Ext(video.VideoPath)
+			if ext != ".m3u8" {
+				update = update.SetVideoPath(fmt.Sprintf("%s/%s-video%s", newRootFolderPath, fileName, ext))
+			} else if ext == ".m3u8" {
+				newHlsVideoRootPath := fmt.Sprintf("%s/%s-video_hls", newRootFolderPath, fileName)
+				update = update.SetVideoPath(fmt.Sprintf("%s/%s-video.m3u8", newHlsVideoRootPath, video.ExtID))
+				update = update.SetVideoHlsPath(newHlsVideoRootPath)
+			}
+		}
+		if video.ThumbnailPath != "" {
+			update = update.SetThumbnailPath(fmt.Sprintf("%s/%s-thumbnail%s", newRootFolderPath, fileName, path.Ext(video.ThumbnailPath)))
+		}
+		if video.WebThumbnailPath != "" {
+			update = update.SetWebThumbnailPath(fmt.Sprintf("%s/%s-web_thumbnail%s", newRootFolderPath, fileName, path.Ext(video.WebThumbnailPath)))
+		}
+		if video.ChatPath != "" {
+			update = update.SetChatPath(fmt.Sprintf("%s/%s-chat%s", newRootFolderPath, fileName, path.Ext(video.ChatPath)))
+		}
+		if video.ChatVideoPath != "" {
+			update = update.SetChatVideoPath(fmt.Sprintf("%s/%s-chat%s", newRootFolderPath, fileName, path.Ext(video.ChatVideoPath)))
+		}
+		if video.LiveChatPath != "" {
+			update = update.SetLiveChatPath(fmt.Sprintf("%s/%s-live-chat%s", newRootFolderPath, fileName, path.Ext(video.LiveChatPath)))
+		}
+		if video.LiveChatConvertPath != "" {
+			update = update.SetLiveChatConvertPath(fmt.Sprintf("%s/%s-live-chat-convert%s", newRootFolderPath, fileName, path.Ext(video.LiveChatConvertPath)))
+		}
+		if video.InfoPath != "" {
+			update = update.SetInfoPath(fmt.Sprintf("%s/%s-info%s", newRootFolderPath, fileName, path.Ext(video.InfoPath)))
+		}
+		if video.CaptionPath != "" {
+			update = update.SetCaptionPath(fmt.Sprintf("%s/%s-caption%s", newRootFolderPath, fileName, path.Ext(video.CaptionPath)))
+		}
+		if len(video.SpriteThumbnailsImages) > 0 {
+			var newSpriteThumbs []string
+			for _, thumb := range video.SpriteThumbnailsImages {
+				newSpriteThumbs = append(newSpriteThumbs, fmt.Sprintf("%s/sprites/%s", newRootFolderPath, path.Base(thumb)))
+			}
+			update = update.SetSpriteThumbnailsImages(newSpriteThumbs)
+		}
+
+		// Save the updates.
+		if _, err := update.Save(context.Background()); err != nil {
+			log.Error().Err(err).Msgf("error updating database paths for video %s", video.ID)
+			tx.Rollback()
+			rollbackRenames(renames)
+			continue
+		}
+
+		if err := tx.Commit(); err != nil {
+			log.Error().Err(err).Msg("error committing transaction")
+			rollbackRenames(renames)
+			continue
+		}
+
+		log.Info().Msgf("migrated video %s to new storage template", video.ID)
+
+		// Remove old root path if it's empty.
+		if err := os.Remove(oldRootFolderPath); err != nil {
+			log.Warn().Err(err).Msgf("error removing old root folder '%s' likely files still exist in there", oldRootFolderPath)
+		}
 
 	}
 
