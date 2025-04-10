8567fce607846be0c3c2060578357e540907e48e
Zibbp <zibbp@zibbp.net>
2025-04-10 01:40:08 +0000

    more translations

--- frontend/app/components/videos/VideoClips.tsx
+++ frontend/app/components/videos/VideoClips.tsx
@@ -2,16 +2,18 @@ import { Video } from "@/app/hooks/useVideos";
 import { Carousel } from "@mantine/carousel";
 import { Title } from "@mantine/core";
 import VideoCard from "./Card";
+import { useTranslations } from "next-intl";
 
 interface Params {
   clips: Video[];
 }
 
 const VideoPageClips = ({ clips }: Params) => {
+  const t = useTranslations("VideoComponents");
 
   return (
     <div>
-      <Title my={5}>Video Clips</Title>
+      <Title my={5}>{t('videoClipsTitle')}</Title>
       <Carousel
         withIndicators
         slideSize={{ base: '100%', sm: '50%', md: '33.333333%', lg: '25%', xl: '16.666666%' }}
