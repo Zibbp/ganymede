55a610c34591d52e1409e5a0449dccf58a3304d8
Zibbp <zibbp@zibbp.net>
2025-06-08 15:14:45 +0000

    fix force login to use runtime env

--- frontend/app/components/authentication/ForceLogin.tsx
+++ frontend/app/components/authentication/ForceLogin.tsx
@@ -3,8 +3,7 @@
 import { useEffect } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import useAuthStore from "@/app/store/useAuthStore";
-
-const isForceLoginEnabled = process.env.NEXT_PUBLIC_FORCE_LOGIN === "true";
+import { env } from "next-runtime-env";
 
 // ForceLogin component checks if the user is logged in and redirects to the login page if not
 export default function ForceLogin({ children }: { children: React.ReactNode }) {
@@ -13,6 +12,8 @@ export default function ForceLogin({ children }: { children: React.ReactNode }) 
   const { isLoggedIn, isLoading } = useAuthStore();
 
   const isAuthPage = pathname === "/login" || pathname === "/register";
+
+  const isForceLoginEnabled = env('NEXT_PUBLIC_REQUIRE_LOGIN')
 
   useEffect(() => {
     if (isForceLoginEnabled && !isLoading && !isLoggedIn && !isAuthPage) {
