b451a72589c26edec37e1d8c9eb3fda516534f64
Flippi <74378751+fllppi@users.noreply.github.com>
2026-04-27 11:49:04 +0200

    feat(frontend): create PlayerAbsoluteTimeIcon component

---
+++ frontend/app/components/videos/PlayerAbsoluteTimeIcon.tsx
@@ -0,0 +1,32 @@
+import { ActionIcon, Tooltip } from "@mantine/core";
+import { IconClock } from "@tabler/icons-react";
+import classes from "./PlayerAbsoluteTimeIcon.module.css"
+import useSettingsStore from "@/app/store/useSettingsStore";
+import { useTranslations } from "next-intl";
+
+const VideoPlayerAbsoluteTimeIcon = () => {
+  const t = useTranslations("VideoComponents")
+  const { setShowAbsoluteTime } = useSettingsStore()
+  const showAbsoluteTime = useSettingsStore((state) => state.showAbsoluteTime);
+
+  const toggleAbsoluteTime = () => {
+    setShowAbsoluteTime(!showAbsoluteTime);
+  };
+  return (
+    <div className={classes.absoluteTimeIcon}>
+      <Tooltip label={t('absoluteTimeIconTooltip')} position="bottom">
+        <ActionIcon
+          size="xl"
+          variant="transparent"
+          onClick={toggleAbsoluteTime}
+          onTouchStart={toggleAbsoluteTime}
+          className={classes.customFullScreenButton}
+        >
+          <IconClock size="1.7rem" />
+        </ActionIcon>
+      </Tooltip>
+    </div>
+  );
+}
+
+export default VideoPlayerAbsoluteTimeIcon;
