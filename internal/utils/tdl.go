93fc277d5bb917e275eed9694098b1d10f57de15
Zibbp <zibbp@zibbp.net>
2026-06-14 13:51:23 +0000

    stream chat input

--- internal/utils/tdl.go
+++ internal/utils/tdl.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -84,10 +85,15 @@ func ConvertTwitchLiveChatToTDLChat(path string, outPath string, channelName str
 
 	log.Debug().Str("chat_file", path).Msg("Converting live Twitch chat to TDL chat for rendering")
 
-	liveComments, err := OpenLiveChatFile(path)
+	liveChatJsonFile, err := os.Open(path)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open chat file: %v", err)
 	}
+	defer func() {
+		if err := liveChatJsonFile.Close(); err != nil {
+			log.Debug().Err(err).Msg("error closing chat file")
+		}
+	}()
 
 	tdlChat := TDLChat{}
 
@@ -96,10 +102,35 @@ func ConvertTwitchLiveChatToTDLChat(path string, outPath string, channelName str
 	tdlChat.Video.ID = previousVideoID // we don't know the video (vod) id at this point
 	tdlChat.Video.Start = 0
 
-	tdlComments := []Comment{}
+	outFile, err := os.Create(outPath)
+	if err != nil {
+		return fmt.Errorf("failed to create parsed comments file: %v", err)
+	}
+	defer func() {
+		if err := outFile.Close(); err != nil {
+			log.Debug().Err(err).Msg("error closing parsed comments file")
+		}
+	}()
+
+	writer := bufio.NewWriter(outFile)
+	defer func() {
+		if err := writer.Flush(); err != nil {
+			log.Debug().Err(err).Msg("error flushing parsed comments file")
+		}
+	}()
+
+	if _, err := fmt.Fprint(writer, `{"streamer":`); err != nil {
+		return fmt.Errorf("failed to write parsed comments: %v", err)
+	}
+	if err := json.NewEncoder(writer).Encode(tdlChat.Streamer); err != nil {
+		return fmt.Errorf("failed to encode streamer: %v", err)
+	}
+	if _, err := fmt.Fprint(writer, `,"comments":[`); err != nil {
+		return fmt.Errorf("failed to write parsed comments: %v", err)
+	}
 
 	// create an initial comment to mark the start of chat
-	tdlComments = append(tdlComments, Comment{
+	initialComment := Comment{
 		ID:                   "546a5e6e-c820-4ad2-9421-9ba5b5bf37ea",
 		Source:               "chat",
 		ContentOffsetSeconds: 0,
@@ -128,217 +159,269 @@ func ConvertTwitchLiveChatToTDLChat(path string, outPath string, channelName str
 				MsgID: nil,
 			},
 		},
-	})
+	}
 
-	for _, liveComment := range liveComments {
-		if liveComment.Message == "" {
+	encoder := json.NewEncoder(writer)
+	if err := encoder.Encode(initialComment); err != nil {
+		return fmt.Errorf("failed to encode initial comment: %v", err)
+	}
+
+	decoder := json.NewDecoder(liveChatJsonFile)
+	token, err := decoder.Token()
+	if err != nil {
+		return fmt.Errorf("failed to read chat file: %v", err)
+	}
+	if delim, ok := token.(json.Delim); !ok || delim != '[' {
+		return fmt.Errorf("failed to read chat file: expected JSON array")
+	}
+
+	lastOffset := initialComment.ContentOffsetSeconds
+	for decoder.More() {
+		var liveComment LiveComment
+		if err := decoder.Decode(&liveComment); err != nil {
+			return fmt.Errorf("failed to decode live comment: %v", err)
+		}
+
+		tdlComment, ok, err := convertLiveCommentToTDLComment(liveComment, chatStartTime)
+		if err != nil {
+			return err
+		}
+		if !ok {
 			continue
 		}
 
-		// get offset in seconds
-		liveCommentUnix, err := microSecondToMillisecondUnix(liveComment.Timestamp)
-		if err != nil {
-			return fmt.Errorf("failed to convert live comment timestamp: %v", err)
+		if _, err := fmt.Fprint(writer, ","); err != nil {
+			return fmt.Errorf("failed to write parsed comments: %v", err)
 		}
-
-		// use chat start time to get offset in seconds
-		diff := liveCommentUnix.Sub(chatStartTime)
-
-		// populate static variables
-		tdlComment := Comment{
-			ContentOffsetSeconds: diff.Seconds(),
-			ID:                   liveComment.MessageID,
-			Source:               "chat",
-			Commenter: Commenter{
-				ID:           liveComment.Author.ID,
-				DisplayName:  liveComment.Author.DisplayName,
-				Name:         liveComment.Author.Name,
-				IsModerator:  liveComment.Author.IsModerator,
-				IsSubscriber: liveComment.Author.IsSubscriber,
-				IsTurbo:      liveComment.Author.IsTurbo,
-			},
-			Message: Message{
-				Body:       liveComment.Message,
-				BitsSpent:  0,
-				UserBadges: []UserBadge{},
-				UserColor:  liveComment.Colour,
-				UserNoticeParams: UserNoticParams{
-					MsgID: nil,
-				},
-			},
+		if err := encoder.Encode(tdlComment); err != nil {
+			return fmt.Errorf("failed to encode comment: %v", err)
 		}
-
-		if liveComment.MessageType == "highlighted_message" {
-			var highlightString = "highlighted-message"
-			tdlComment.Message.UserNoticeParams.MsgID = &highlightString
-		}
-
-		// create the first message fragment
-		tdlComment.Message.Fragments = append(tdlComment.Message.Fragments, Fragment{
-			Text:     liveComment.Message,
-			Emoticon: nil,
-		})
-
-		// set default offset value for this live comment
-		message_is_offset := false
-
-		// parse emotes, creating fragments with positions
-		emoteFragments := []Fragment{}
-		if liveComment.Emotes != nil {
-			for _, liveCommentEmote := range liveComment.Emotes {
-				for i, liveCommentEmoteLocation := range liveCommentEmote.Locations {
-					var pos1, pos2 int
-					var emoteFragment Fragment
-					// get position of emote in message
-					emotePositions := strings.Split(liveCommentEmoteLocation, "-")
-					pos1, err := strconv.Atoi(emotePositions[0])
-					if err != nil {
-						return fmt.Errorf("failed to convert emote position: %v", err)
-					}
-					chatPos2, err := strconv.Atoi(emotePositions[1])
-					if err != nil {
-						return fmt.Errorf("failed to convert emote position: %v", err)
-					}
-					pos2 = pos1 + len(liveCommentEmote.Name)
-
-					var slicedEmote string
-
-					// Check if pos1 and pos2 are within bounds
-					if pos1 < 0 || pos2 > len(liveComment.Message) {
-						// Check if pos1 is negative and pos2 is within bounds
-						if pos1 < 0 || chatPos2 > len(liveComment.Message) {
-							log.Error().Str("message_id", liveComment.MessageID).Msg("emote position out of bounds, skipping emote")
-							continue
-						}
-						// If default chat pos2 is in bounds use it
-						log.Warn().Str("message_id", liveComment.MessageID).Msg("emote position out of bounds, using default chat pos2 instead")
-						slicedEmote = liveComment.Message[pos1:chatPos2]
-					} else {
-						slicedEmote = liveComment.Message[pos1:pos2]
-					}
-
-					// ensure that the sliced string equals the emote
-					// sometimes the output of chat-downloader will not include a unicode character when calculating positions causing an offset in positions
-					if slicedEmote != liveCommentEmote.Name || message_is_offset {
-						log.Debug().Str("message_id", liveComment.MessageID).Msg("emote position mismatch detected while converting chat")
-						message_is_offset = true
-
-						// attempt to get emote position in comment message
-						pos1, pos2, found := findSubstringPositions(liveComment.Message, liveCommentEmote.Name, i+1)
-						if !found {
-							log.Warn().Str("message_id", liveComment.MessageID).Msg("unable to extract emote positions from message, skpping")
-							continue
-						}
-						slicedEmote = liveComment.Message[pos1:pos2]
-						emoteFragment = Fragment{
-							Pos1: pos1,
-							Pos2: pos2,
-							Text: slicedEmote,
-							Emoticon: &Emoticon{
-								EmoticonID:    liveCommentEmote.ID,
-								EmoticonSetID: "",
-							},
-						}
-					} else {
-						emoteFragment = Fragment{
-							Pos1: pos1,
-							Pos2: pos2,
-							Text: slicedEmote,
-							Emoticon: &Emoticon{
-								EmoticonID:    liveCommentEmote.ID,
-								EmoticonSetID: "",
-							},
-						}
-					}
-
-					emoteFragments = append(emoteFragments, emoteFragment)
-				}
-			}
-		}
-
-		// sort emote fragments by position ascending
-		sort.Slice(emoteFragments, func(i, j int) bool {
-			return emoteFragments[i].Pos1 < emoteFragments[j].Pos1
-		})
-
-		formattedEmoteFragments := []Fragment{}
-
-		// remove emote fragments from message fragments
-		for i, emoteFragment := range emoteFragments {
-			if i == 0 {
-				fragmentText := tdlComment.Message.Body[:emoteFragment.Pos1]
-				fragment := Fragment{
-					Text:     fragmentText,
-					Emoticon: nil,
-				}
-				formattedEmoteFragments = append(formattedEmoteFragments, fragment)
-				formattedEmoteFragments = append(formattedEmoteFragments, emoteFragment)
-			} else {
-				if emoteFragment.Pos1 == 0 {
-					log.Warn().Str("message_id", liveComment.MessageID).Msg("skipping invalid emote position")
-					continue
-				}
-				fragmentText := tdlComment.Message.Body[emoteFragments[i-1].Pos2:emoteFragment.Pos1]
-				fragment := Fragment{
-					Text:     fragmentText,
-					Emoticon: nil,
-				}
-				formattedEmoteFragments = append(formattedEmoteFragments, fragment)
-				formattedEmoteFragments = append(formattedEmoteFragments, emoteFragment)
-			}
-		}
-
-		// check if last fragment is an emoticon
-		if len(formattedEmoteFragments) > 0 {
-			lastItem := len(formattedEmoteFragments) - 1
-			if formattedEmoteFragments[lastItem].Emoticon.EmoticonID != "" {
-				fragmentText := tdlComment.Message.Body[formattedEmoteFragments[lastItem].Pos2:]
-				fragment := Fragment{
-					Text:     fragmentText,
-					Emoticon: nil,
-				}
-				formattedEmoteFragments = append(formattedEmoteFragments, fragment)
-			}
-		}
-
-		// ensure message has emote fragments
-		if len(formattedEmoteFragments) > 0 {
-			tdlComment.Message.Fragments = formattedEmoteFragments
-		}
-
-		// user badges
-		if len(liveComment.Author.Badges) > 0 {
-			for _, liveCommentBadge := range liveComment.Author.Badges {
-				liveCommentUserBadge := UserBadge{
-					ID:      liveCommentBadge.Name,
-					Version: fmt.Sprintf("%v", liveCommentBadge.Version),
-				}
-				tdlComment.Message.UserBadges = append(tdlComment.Message.UserBadges, liveCommentUserBadge)
-			}
-		}
-
-		// ensure user has a display name color
-		if tdlComment.Message.UserColor == "" {
-			tdlComment.Message.UserColor = "#a65ee8"
-		}
-
-		tdlComments = append(tdlComments, tdlComment)
+		lastOffset = tdlComment.ContentOffsetSeconds
 	}
 
-	tdlChat.Comments = tdlComments
-
-	// get last comment offset and set as video end
-	lastComment := tdlChat.Comments[len(tdlChat.Comments)-1]
-	tdlChat.Video.End = int64(lastComment.ContentOffsetSeconds)
-
-	// write chat
-	err = writeTDLChat(tdlChat, outPath)
+	token, err = decoder.Token()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read chat file: %v", err)
+	}
+	if delim, ok := token.(json.Delim); !ok || delim != ']' {
+		return fmt.Errorf("failed to read chat file: expected end of JSON array")
+	}
+
+	tdlChat.Video.End = int64(lastOffset)
+	if _, err := fmt.Fprint(writer, `],"video":`); err != nil {
+		return fmt.Errorf("failed to write parsed comments: %v", err)
+	}
+	if err := encoder.Encode(tdlChat.Video); err != nil {
+		return fmt.Errorf("failed to encode video: %v", err)
+	}
+	if _, err := fmt.Fprint(writer, "}"); err != nil {
+		return fmt.Errorf("failed to write parsed comments: %v", err)
+	}
+	if err := writer.Flush(); err != nil {
+		return fmt.Errorf("failed to write parsed comments: %v", err)
 	}
 
 	return nil
 
+}
+
+func convertLiveCommentToTDLComment(liveComment LiveComment, chatStartTime time.Time) (Comment, bool, error) {
+	if liveComment.Message == "" {
+		return Comment{}, false, nil
+	}
+
+	// get offset in seconds
+	liveCommentUnix, err := microSecondToMillisecondUnix(liveComment.Timestamp)
+	if err != nil {
+		return Comment{}, false, fmt.Errorf("failed to convert live comment timestamp: %v", err)
+	}
+
+	// use chat start time to get offset in seconds
+	diff := liveCommentUnix.Sub(chatStartTime)
+
+	// populate static variables
+	tdlComment := Comment{
+		ContentOffsetSeconds: diff.Seconds(),
+		ID:                   liveComment.MessageID,
+		Source:               "chat",
+		Commenter: Commenter{
+			ID:           liveComment.Author.ID,
+			DisplayName:  liveComment.Author.DisplayName,
+			Name:         liveComment.Author.Name,
+			IsModerator:  liveComment.Author.IsModerator,
+			IsSubscriber: liveComment.Author.IsSubscriber,
+			IsTurbo:      liveComment.Author.IsTurbo,
+		},
+		Message: Message{
+			Body:       liveComment.Message,
+			BitsSpent:  0,
+			UserBadges: []UserBadge{},
+			UserColor:  liveComment.Colour,
+			UserNoticeParams: UserNoticParams{
+				MsgID: nil,
+			},
+		},
+	}
+
+	if liveComment.MessageType == "highlighted_message" {
+		var highlightString = "highlighted-message"
+		tdlComment.Message.UserNoticeParams.MsgID = &highlightString
+	}
+
+	// create the first message fragment
+	tdlComment.Message.Fragments = append(tdlComment.Message.Fragments, Fragment{
+		Text:     liveComment.Message,
+		Emoticon: nil,
+	})
+
+	// set default offset value for this live comment
+	message_is_offset := false
+
+	// parse emotes, creating fragments with positions
+	emoteFragments := []Fragment{}
+	if liveComment.Emotes != nil {
+		for _, liveCommentEmote := range liveComment.Emotes {
+			for i, liveCommentEmoteLocation := range liveCommentEmote.Locations {
+				var pos1, pos2 int
+				var emoteFragment Fragment
+				// get position of emote in message
+				emotePositions := strings.Split(liveCommentEmoteLocation, "-")
+				if len(emotePositions) != 2 {
+					log.Warn().Str("message_id", liveComment.MessageID).Msg("invalid emote position, skipping emote")
+					continue
+				}
+				pos1, err := strconv.Atoi(emotePositions[0])
+				if err != nil {
+					return Comment{}, false, fmt.Errorf("failed to convert emote position: %v", err)
+				}
+				chatPos2, err := strconv.Atoi(emotePositions[1])
+				if err != nil {
+					return Comment{}, false, fmt.Errorf("failed to convert emote position: %v", err)
+				}
+				pos2 = pos1 + len(liveCommentEmote.Name)
+
+				var slicedEmote string
+
+				// Check if pos1 and pos2 are within bounds
+				if pos1 < 0 || pos2 > len(liveComment.Message) {
+					// Check if pos1 is negative and pos2 is within bounds
+					if pos1 < 0 || chatPos2 > len(liveComment.Message) {
+						log.Error().Str("message_id", liveComment.MessageID).Msg("emote position out of bounds, skipping emote")
+						continue
+					}
+					// If default chat pos2 is in bounds use it
+					log.Warn().Str("message_id", liveComment.MessageID).Msg("emote position out of bounds, using default chat pos2 instead")
+					slicedEmote = liveComment.Message[pos1:chatPos2]
+				} else {
+					slicedEmote = liveComment.Message[pos1:pos2]
+				}
+
+				// ensure that the sliced string equals the emote
+				// sometimes the output of chat-downloader will not include a unicode character when calculating positions causing an offset in positions
+				if slicedEmote != liveCommentEmote.Name || message_is_offset {
+					log.Debug().Str("message_id", liveComment.MessageID).Msg("emote position mismatch detected while converting chat")
+					message_is_offset = true
+
+					// attempt to get emote position in comment message
+					pos1, pos2, found := findSubstringPositions(liveComment.Message, liveCommentEmote.Name, i+1)
+					if !found {
+						log.Warn().Str("message_id", liveComment.MessageID).Msg("unable to extract emote positions from message, skpping")
+						continue
+					}
+					slicedEmote = liveComment.Message[pos1:pos2]
+					emoteFragment = Fragment{
+						Pos1: pos1,
+						Pos2: pos2,
+						Text: slicedEmote,
+						Emoticon: &Emoticon{
+							EmoticonID:    liveCommentEmote.ID,
+							EmoticonSetID: "",
+						},
+					}
+				} else {
+					emoteFragment = Fragment{
+						Pos1: pos1,
+						Pos2: pos2,
+						Text: slicedEmote,
+						Emoticon: &Emoticon{
+							EmoticonID:    liveCommentEmote.ID,
+							EmoticonSetID: "",
+						},
+					}
+				}
+
+				emoteFragments = append(emoteFragments, emoteFragment)
+			}
+		}
+	}
+
+	// sort emote fragments by position ascending
+	sort.Slice(emoteFragments, func(i, j int) bool {
+		return emoteFragments[i].Pos1 < emoteFragments[j].Pos1
+	})
+
+	formattedEmoteFragments := []Fragment{}
+
+	// remove emote fragments from message fragments
+	for i, emoteFragment := range emoteFragments {
+		if i == 0 {
+			fragmentText := tdlComment.Message.Body[:emoteFragment.Pos1]
+			fragment := Fragment{
+				Text:     fragmentText,
+				Emoticon: nil,
+			}
+			formattedEmoteFragments = append(formattedEmoteFragments, fragment)
+			formattedEmoteFragments = append(formattedEmoteFragments, emoteFragment)
+		} else {
+			if emoteFragment.Pos1 == 0 {
+				log.Warn().Str("message_id", liveComment.MessageID).Msg("skipping invalid emote position")
+				continue
+			}
+			fragmentText := tdlComment.Message.Body[emoteFragments[i-1].Pos2:emoteFragment.Pos1]
+			fragment := Fragment{
+				Text:     fragmentText,
+				Emoticon: nil,
+			}
+			formattedEmoteFragments = append(formattedEmoteFragments, fragment)
+			formattedEmoteFragments = append(formattedEmoteFragments, emoteFragment)
+		}
+	}
+
+	// check if last fragment is an emoticon
+	if len(formattedEmoteFragments) > 0 {
+		lastItem := len(formattedEmoteFragments) - 1
+		if formattedEmoteFragments[lastItem].Emoticon != nil && formattedEmoteFragments[lastItem].Emoticon.EmoticonID != "" {
+			fragmentText := tdlComment.Message.Body[formattedEmoteFragments[lastItem].Pos2:]
+			fragment := Fragment{
+				Text:     fragmentText,
+				Emoticon: nil,
+			}
+			formattedEmoteFragments = append(formattedEmoteFragments, fragment)
+		}
+	}
+
+	// ensure message has emote fragments
+	if len(formattedEmoteFragments) > 0 {
+		tdlComment.Message.Fragments = formattedEmoteFragments
+	}
+
+	// user badges
+	if len(liveComment.Author.Badges) > 0 {
+		for _, liveCommentBadge := range liveComment.Author.Badges {
+			liveCommentUserBadge := UserBadge{
+				ID:      liveCommentBadge.Name,
+				Version: fmt.Sprintf("%v", liveCommentBadge.Version),
+			}
+			tdlComment.Message.UserBadges = append(tdlComment.Message.UserBadges, liveCommentUserBadge)
+		}
+	}
+
+	// ensure user has a display name color
+	if tdlComment.Message.UserColor == "" {
+		tdlComment.Message.UserColor = "#a65ee8"
+	}
+
+	return tdlComment, true, nil
 }
 
 func writeTDLChat(parsedChat TDLChat, outPath string) error {
